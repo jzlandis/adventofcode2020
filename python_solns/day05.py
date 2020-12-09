ca11ee6a6d8233f0a9f570b670b7fd94962700da
Jeremy Z. Landis <j.z.landis@hotmail.com>
2020-12-08 20:44:11 -0600

    day05 and day06 solutions

---
+++ python_solns/day05.py
@@ -0,0 +1,44 @@
+import sys
+
+
+def make_parser(ispan, lhalf):
+    def parse_row(code, span=ispan):
+        if len(code) == 1:
+            if code[0] == lhalf:
+                return int(span[0])
+            else:
+                return int(span[1]-1)
+        else:
+            if code[0] == lhalf:
+                span = (span[0], sum(span)/2)
+            else:
+                span = (sum(span)/2, span[1])
+            return parse_row(code[1:], span=span)
+    return parse_row
+
+
+parse_row = make_parser((0, 128), 'F')
+parse_column = make_parser((0, 8), 'L')
+
+
+def main(f, part):
+    outcomes = []
+    for line in open(f):
+        row = parse_row(line.strip()[:7])
+        column = parse_column(line.strip()[7:])
+        ID = row*8 + column
+        outcomes.append((line.strip(), row, column, ID))
+    if part == 'part1':
+        print(max(outcomes, key=lambda x: x[3]))
+    if part == 'part2':
+        IDs = sorted(x[-1] for x in outcomes)
+        for i in range(IDs[0], IDs[-1]+1):
+            if not i in IDs and i-1 in IDs and i+1 in IDs:
+                print(i)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        main(sys.argv[2], sys.argv[1])
+    else:
+        sys.stderr.write('USAGE: python day05.py <part1|part2> <input_file>\n')
---
+++ python_solns/day06.py
@@ -0,0 +1,50 @@
+import sys
+
+
+def group_generator(f):
+    group = []
+    for line in open(f):
+        if line.strip() == '':
+            if len(group) != 0:
+                yield group
+            group = []
+        else:
+            group.append(line.strip())
+    if len(group) != 0:
+        yield group
+
+
+def groups2sets1(groups):
+    for group in groups:
+        yess = set()
+        for answers in group:
+            for answer in answers:
+                yess.add(answer)
+        yield yess
+
+
+def groups2sets2(groups):
+    for group in groups:
+        sets = []
+        for answers in group:
+            sets.append(set(answers))
+        super_set = sets[0]
+        for s in sets[1:]:
+            super_set = super_set.intersection(s)
+        yield super_set
+
+
+def main(f, part):
+    if part == 'part1':
+        groups2sets = groups2sets1
+    if part == 'part2':
+        groups2sets = groups2sets2
+    group_answers = list(groups2sets(group_generator(f)))
+    print(sum(len(g) for g in group_answers))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        main(sys.argv[2], sys.argv[1])
+    else:
+        sys.stderr.write('USAGE: python day06.py <part1|part2> <input_file>\n')
